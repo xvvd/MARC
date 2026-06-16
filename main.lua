f2c86f27c7f21446cc1696c24b12cd606dbdc038
xvvd <eearboot@gmail.com>
2026-06-16 12:08:26 -0400

    Update main.lua
    Removed setTheme function

--- main.lua
+++ main.lua
@@ -1,20 +1,9 @@
 local ui = require("ui")
 
-local function setTheme(style)
-    ui.theme = string.format("`{style}`")
-end
-
 ui.theme = "dark"
 
 local win = ui.Window("MARC [MADNESS, Asset, Repository, Client]", "fixed", 400, 200)
-local label = ui.Label(win, "Hello! MARC is in Alpha!", 40, 80)
-
-local button = ui.Button(win, "Change Theme (Will change to lightmode)", 40, 120)
-button.onClick = function()
-    setTheme("light")
-end
-
-
+local label = ui.Label(win, "Hello! MARC is in Alpha!", 40, 100)
 
 win:show()
 
